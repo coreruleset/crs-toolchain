ef0a413a9f90e7773548d07020d197085b64bd7c
Felipe Zipitria <felipe.zipitria@owasp.org>
2022-10-24 09:06:30 -0300

    test: add multi file include test suite
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -27,6 +27,7 @@ func (s *parserTestSuite) SetupTest() {
 func TestRunParserTestSuite(t *testing.T) {
 	suite.Run(t, new(parserTestSuite))
 	suite.Run(t, new(parserIncludeTestSuite))
+	suite.Run(t, new(parserMultiIncludeTestSuite))
 }
 
 func (s *parserTestSuite) TestParser_NewParser() {
@@ -72,13 +73,54 @@ func (s *parserIncludeTestSuite) SetupSuite() {
 
 func (s *parserIncludeTestSuite) TearDownSuite() {
 	s.includeFile.Close()
-	os.Remove("test.data")
+	os.Remove(s.includeFile.Name())
 }
 
 func (s *parserIncludeTestSuite) TestParserInclude_FromFile() {
 	parser := NewParser(s.reader)
 	actual, n := parser.Parse()
 	expected := bytes.NewBufferString("This data comes from the included file.\n##! This is a comment line.\n")
+
+	s.Equal(expected.String(), actual.String())
+	s.Equal(expected.Len(), n)
+}
+
+// Test Suite to perform multiple inclusions
+
+type parserMultiIncludeTestSuite struct {
+	suite.Suite
+	reader      io.Reader
+	includeFile []*os.File
+}
+
+func (s *parserMultiIncludeTestSuite) SetupSuite() {
+	for i := 0; i < 4; i++ {
+		file, err := os.CreateTemp(os.TempDir(), "multi-include.data")
+		s.NoError(err, "couldn't create %s file", file.Name())
+		if i == 0 {
+			// Only the initial include goes to the reader
+			s.reader = strings.NewReader(fmt.Sprintf("##!> include %s\n##! This is comment %d.\n", file.Name(), i))
+		}
+		s.includeFile = append(s.includeFile, file)
+		// Write to file i-1
+		if i > 0 {
+			_, err := s.includeFile[i-1].WriteString(fmt.Sprintf("##!> include %s\n##! This is comment %d.\n", file.Name(), i))
+			s.NoError(err, "writing temp include file failed")
+		}
+	}
+}
+
+func (s *parserMultiIncludeTestSuite) TearDownSuite() {
+	for i := 0; i < 4; i++ {
+		s.includeFile[i].Close()
+		os.Remove(s.includeFile[i].Name())
+	}
+}
+
+func (s *parserMultiIncludeTestSuite) TestParserMultiInclude_FromMultiFile() {
+	parser := NewParser(s.reader)
+	actual, n := parser.Parse()
+	expected := bytes.NewBufferString("##! This is comment 3.\n##! This is comment 2.\n##! This is comment 1.\n##! This is comment 0.\n")
 
 	s.Equal(expected.String(), actual.String())
 	s.Equal(expected.Len(), n)
