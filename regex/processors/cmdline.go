c2ed9fbe2f344ae3692ecb4266b78a8acb4760e4
Max Leske <maxleske@gmail.com>
2022-11-05 22:11:14 +0100

    wip: don'd use flags for meta characters
    Fix a bunch of things that didn't work

--- regex/processors/cmdline.go
+++ regex/processors/cmdline.go
@@ -99,7 +99,7 @@ func NewCmdLine(ctx *Context, cmdType CmdLineType) *CmdLine {
 	return a
 }
 
-// ProcessLine applies the processors logic to a single line
+// ProcessLine implements the line processor
 func (c *CmdLine) ProcessLine(line string) error {
 	if len(line) != 0 {
 		processed := c.regexpStr(line)
@@ -110,7 +110,7 @@ func (c *CmdLine) ProcessLine(line string) error {
 	return nil
 }
 
-// Complete finalizes the processor, producing its output
+// Complete is the class method
 func (c *CmdLine) Complete() ([]string, error) {
 	assembly, err := rassemble.Join(c.proc.lines)
 	if err != nil {
@@ -118,16 +118,6 @@ func (c *CmdLine) Complete() ([]string, error) {
 	}
 	logger.Trace().Msgf("cmdLine Complete result: %v", assembly)
 	return []string{assembly}, nil
-}
-
-// Consume applies the state of a nested processor
-func (c *CmdLine) Consume(lines []string) error {
-	for _, line := range lines {
-		if err := c.ProcessLine(line); err != nil {
-			return err
-		}
-	}
-	return nil
 }
 
 // regexpStr converts a single line to regexp format, and insert anti-cmdline
