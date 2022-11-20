c67eb97d5e0f8d7e1be1591bce2219b94b1726a8
Felipe Zipitria <felipe.zipitria@owasp.org>
2022-11-20 10:27:29 -0300

    feat: print using levels
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

--- logger/logger.go
+++ logger/logger.go
@@ -27,9 +27,28 @@ func init() {
 // Another possibility is to add the following strings between the level and the message:
 // file={name},line={line},endLine={endLine},title={title}
 func SetGithubOutput() zerolog.Logger {
-	// the following formatlevel func might need to rename levels to match with github naming
+	// the following formatlevel loosely translates from posix levels to github levels
 	consoleOutput.FormatLevel = func(i interface{}) string {
-		return fmt.Sprintf("::%s", i)
+		var l string
+		if ll, ok := i.(string); ok {
+			switch ll {
+			case zerolog.LevelTraceValue, zerolog.LevelDebugValue:
+				l = "debug"
+			case zerolog.LevelInfoValue:
+				l = "notice "
+			case zerolog.LevelWarnValue:
+				l = "warn "
+			case zerolog.LevelErrorValue, zerolog.LevelFatalValue, zerolog.LevelPanicValue:
+				l = "error "
+			default:
+				l = "???"
+			}
+		} else {
+			if i == nil {
+				l = "???"
+			}
+		}
+		return fmt.Sprintf("::%s", l)
 	}
 	consoleOutput.FormatMessage = func(i interface{}) string {
 		return fmt.Sprintf("::%s", i)
