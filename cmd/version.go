a5bf7f185c152f964284ceeff63a7ce2b15363c4
Felipe Zipitria <felipe.zipitria@owasp.org>
2023-05-21 12:59:19 -0300

    feat: do not run version check when running in CI
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

--- cmd/version.go
+++ cmd/version.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 
@@ -21,12 +22,15 @@ var versionCmd = &cobra.Command{
 	Long:  `All software has versions. This is crs-toolchain's`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("crs-toolchain", rootCmd.Version)
-		latest, err := updater.LatestVersion()
-		if err != nil {
-			logger.Error().Err(err).Msg("Failed to check for updates")
-		} else if latest != "" {
-			fmt.Println("Latest version is:", latest)
-			fmt.Println("Run 'crs-toolchain self-update' to update")
+		// do not run when in CI (e.g. GitHub Actions)
+		if os.Getenv("CI") != "true" {
+			latest, err := updater.LatestVersion()
+			if err != nil {
+				logger.Error().Err(err).Msg("Failed to check for updates")
+			} else if latest != "" {
+				fmt.Println("Latest version is:", latest)
+				fmt.Println("Run 'crs-toolchain self-update' to update")
+			}
 		}
 	},
 }
