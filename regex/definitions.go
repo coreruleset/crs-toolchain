2982cbce891c86068bbf2cfa008bb170f6c5b26d
Felipe Zipitria <felipe.zipitria@owasp.org>
2024-03-02 14:49:53 -0300

    fix: capture test title
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

--- regex/definitions.go
+++ regex/definitions.go
@@ -72,7 +72,7 @@ var RuleIdTestFileNameRegex = regexp.MustCompile(`^(\d{6})(?:\.ya?ml)?$`)
 
 // TestTitleRegex matches any test_title line in test YAML files (test_title: "<title>").
 // Everything up to the value of the test title is captured in group 1, test title in group 2.
-var TestTitleRegex = regexp.MustCompile(`(.*test_title:).*$`)
+var TestTitleRegex = regexp.MustCompile(`(.*test_title:)\s+(.*$)`)
 
 // DefinitionReferenceRegex matches any reference to a definition.
 // The matched reference name will be captured in group 1.
