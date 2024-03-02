43331f632020e13b7de23ad0735058a776a5993f
Felipe Zipitria <felipe.zipitria@owasp.org>
2024-03-02 14:40:24 -0300

    fix: use logger only for github messages
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

--- logger/logger.go
+++ logger/logger.go
@@ -5,6 +5,7 @@ package logger
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/rs/zerolog"
@@ -13,31 +14,23 @@ import (
 
 const DefaultLogLevel zerolog.Level = zerolog.InfoLevel
 
-var consoleOutput = zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "03:04:05"}
-
 func init() {
-	log.Logger = log.Output(consoleOutput).With().Caller().Logger()
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "03:04:05"}).With().Caller().Logger()
 	zerolog.SetGlobalLevel(DefaultLogLevel)
 }
 
-// SetGithubOutput changes the standard logging format to be compatible with GitHub's.
-// See https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#example-creating-an-annotation-for-an-error
-// Levels on github are:
-// - debug, notice, error, warning
-// Another possibility is to add the following strings between the level and the message:
-// file={name},line={line},endLine={endLine},title={title}
-func SetGithubOutput() zerolog.Logger {
-	// the following formatlevel loosely translates from posix levels to github levels
-	consoleOutput.FormatLevel = func(i interface{}) string {
+func SetGithubOutput(w io.Writer) zerolog.Logger {
+	ghOutput := zerolog.ConsoleWriter{Out: w, TimeFormat: "03:04:05"}
+	ghOutput.FormatLevel = func(i interface{}) string {
 		var l string
 		if ll, ok := i.(string); ok {
 			switch ll {
 			case zerolog.LevelTraceValue, zerolog.LevelDebugValue:
 				l = "debug"
 			case zerolog.LevelInfoValue:
-				l = "notice "
+				l = "notice"
 			case zerolog.LevelWarnValue:
-				l = "warn "
+				l = "warn"
 			case zerolog.LevelErrorValue, zerolog.LevelFatalValue, zerolog.LevelPanicValue:
 				l = "error "
 			default:
@@ -50,9 +43,15 @@ func SetGithubOutput() zerolog.Logger {
 		}
 		return fmt.Sprintf("::%s", l)
 	}
-	consoleOutput.FormatMessage = func(i interface{}) string {
-		return fmt.Sprintf("::%s", i)
+	ghOutput.FormatMessage = func(i interface{}) string {
+		return fmt.Sprintf("::%s\n", i)
 	}
-	consoleOutput.PartsExclude = []string{zerolog.TimestampFieldName}
-	return zerolog.New(consoleOutput).With().Logger()
+	ghOutput.PartsExclude = []string{zerolog.TimestampFieldName, zerolog.CallerFieldName}
+	ghOutput.PartsOrder = []string{
+		zerolog.LevelFieldName,
+		zerolog.MessageFieldName,
+	}
+	ghOutput.NoColor = true
+
+	return log.Output(ghOutput).With().Caller().Logger()
 }
