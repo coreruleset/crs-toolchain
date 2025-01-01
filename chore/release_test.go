3c66277e6a6c9b41b2a25929232540cca0ac95fe
Max Leske <250711+theseion@users.noreply.github.com>
2025-01-01 17:20:49 +0100

    Release v1.2.3

--- chore/release_test.go
+++ chore/release_test.go
@@ -1,13 +1,14 @@
 package chore
 
 import (
+	"os"
+	"os/exec"
 	"testing"
-	"time"
 
+	"github.com/Masterminds/semver/v3"
 	"github.com/coreruleset/crs-toolchain/v2/context"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
-	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -18,21 +19,14 @@ type choreReleaseTestSuite struct {
 
 func (s *choreReleaseTestSuite) SetupTest() {
 	s.repoDir = s.T().TempDir()
-	repo, err := git.PlainInit(s.repoDir, false)
+	cmd := exec.Command("git", "init", "-b", "main")
+	cmd.Dir = s.repoDir
+	err := cmd.Run()
 	s.Require().NoError(err)
 
-	w, err := repo.Worktree()
-	s.Require().NoError(err)
-	commit, err := w.Commit("dummy", &git.CommitOptions{
-		Author: &object.Signature{
-			Name:  "Homer Simpson",
-			Email: "doughnuts@simpsons.com",
-			When:  time.Now(),
-		},
-		AllowEmptyCommits: true,
-	})
-	s.Require().NoError(err)
-	_, err = repo.CommitObject(commit)
+	cmd = exec.Command("git", "commit", "--allow-empty", "-m", "dummy")
+	cmd.Dir = s.repoDir
+	err = cmd.Run()
 	s.Require().NoError(err)
 }
 
@@ -40,20 +34,65 @@ func TestRunChoreReleaseTestSuite(t *testing.T) {
 	suite.Run(t, new(choreReleaseTestSuite))
 }
 
-func (s *choreReleaseTestSuite) TestCreateBranch() {
+func (s *choreReleaseTestSuite) TestCreateAndcheckoutBranch() {
 	branchName := "v1.2.3"
 	ctxt := context.New(s.repoDir, "")
-	createAndCheckOutBranch(ctxt, branchName)
+	createAndCheckOutBranch(ctxt, branchName, "main")
 	repo, err := git.PlainOpen(s.repoDir)
 	s.Require().NoError(err)
 
-	branch, err := repo.Branch(branchName)
-	s.Require().NoError(err, "Branch should have been created")
+	branches, err := repo.Branches()
+	s.Require().NoError(err)
+
+	found := false
+	var branchRef *plumbing.Reference
+	branches.ForEach(func(r *plumbing.Reference) error {
+		if r.Name().Short() == branchName {
+			found = true
+			branchRef = r
+		}
+		return nil
+	})
+	s.True(found)
 
 	headRef, err := repo.Head()
 	s.Require().NoError(err)
 
-	branchHash, err := repo.ResolveRevision(plumbing.Revision(branch.Name))
+	s.Equal(branchRef.Hash(), headRef.Hash(), "New branch should have been checked out")
+}
+
+func (s *choreReleaseTestSuite) TestCreateCommit() {
+	branchName := "v1.2.3"
+	version, err := semver.NewVersion(branchName)
 	s.Require().NoError(err)
-	s.Equal(*branchHash, headRef.Hash(), "New branch should have been checked out")
+	ctxt := context.New(s.repoDir, "")
+	createAndCheckOutBranch(ctxt, branchName, "main")
+
+	// Add something to commit, as `createCommit` doesn't allow empty commits
+	os.WriteFile(s.repoDir+"file", []byte("content"), os.ModePerm)
+	cmd := exec.Command("git", "add", ".")
+	err = cmd.Run()
+	s.Require().NoError(err)
+
+	createCommit(ctxt, version)
+
+	repo, err := git.PlainOpen(s.repoDir)
+	s.Require().NoError(err)
+	worktree, err := repo.Worktree()
+	s.Require().NoError(err)
+	status, err := worktree.Status()
+	s.Require().NoError(err)
+	s.True(status.IsClean())
+
+	revision, err := repo.ResolveRevision("HEAD")
+	s.Require().NoError(err)
+	commit, err := repo.CommitObject(*revision)
+	s.Require().NoError(err)
+	s.Equal("Release "+branchName, commit.Message)
+
+	parent, err := commit.Parent(0)
+	s.Require().NoError(err)
+	branch, err := repo.ResolveRevision(plumbing.Revision(branchName))
+	s.Require().NoError(err)
+	s.Equal(*branch, parent.Hash)
 }
