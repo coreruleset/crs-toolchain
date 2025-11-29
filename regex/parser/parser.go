451f09300d22c391bd02eecbf624a0e71cedc7cb
Max Leske <250711+theseion@users.noreply.github.com>
2025-11-29 20:09:01 +0100

    fix: skip subdirectories when running `update --all`

--- regex/parser/parser.go
+++ regex/parser/parser.go
@@ -278,7 +278,7 @@ func parseFile(rootParser *Parser, filename string, definitions map[string]strin
 		newP.variables = definitions
 	}
 	out, _ := newP.Parse(false)
-	newOut, err := mergePrefixesSuffixes(rootParser, newP, out)
+	newOut, err := mergePrefixesSuffixes(newP, out)
 	if err != nil {
 		logger.Fatal().Msgf("error parsing file: %v", err.Error())
 	}
@@ -288,8 +288,8 @@ func parseFile(rootParser *Parser, filename string, definitions map[string]strin
 
 // Merge prefixes, and suffixes from include files into another parser.
 // All of these need to be treated as local to the source parser.
-// We removed flag merging because of https://github.com/coreruleset/crs-toolchain/v2/issues/72
-func mergePrefixesSuffixes(target *Parser, source *Parser, out *bytes.Buffer) (*bytes.Buffer, error) {
+// We removed flag merging because of https://github.com/coreruleset/crs-toolchain/issues/72
+func mergePrefixesSuffixes(source *Parser, out *bytes.Buffer) (*bytes.Buffer, error) {
 	logger.Trace().Msg("merging prefixes, suffixes from included file")
 	// If the included file has flags, this is an error
 	if len(source.Flags) > 0 {
